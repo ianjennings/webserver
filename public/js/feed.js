4219adb137b97d0c3d8b452bbc1296bab1c8fa83
Marak Squires <marak.squires@gmail.com>
2011-08-11 01:14:38 -0700

    [refactor]: Migrating to Hook.io 0.6.x API. Removed a lot of cruff from browser demo.

--- public/js/feed.js
+++ public/js/feed.js
@@ -13,28 +13,7 @@ $(document).ready(function(){
 });
 
 function renderFeed(event, data) {
-  
-  var li = '';
-
-  switch(event) {
-
-    case 'o.log':
-      li = renderLog(data);
-    break;
-
-    case 'o.request':
-      li = renderRequest(data);
-    break;
-
-    default:
-    break;
-
-  }
-
-  var levelClass = (data.level || 'info');
-
-  customPrepend('#status-feed', '<li class = "' + levelClass + '">' + li + '</li>');
-
+  customPrepend('#status-feed', '<li>' + event + '</li>');
 };
 
 
@@ -67,56 +46,3 @@ function customPrepend(ul, li) {
   });
   
 }
-
-function renderRequest(request) {
-  
-  var str = '';
-  
-  var date = new Date();
-  
-  var dateStr = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getHours() + ':' + date.getMinutes() + ':'+ date.getSeconds();
-
-  str += '<span class = "url">';
-  str += request.url;
-  str += '</span>';
-
-  str += '<span class = "timestamp">';
-  str += dateStr;
-  str += '</span>';
-
-  return str;
-
-}
-
-function renderLog(logEvent) {
-
-  var str = '';
-  try {
-    str += '<span class = "url">';
-    str +=   logEvent.url;
-    str += '</span>';
-    str += '<span class = "msg">';
-    str +=   logEvent.msg;
-    str += '</span>';
-    str += '<span class = "timestamp">';
-    str +=   logEvent.timestamp;
-    str += '</span>';
-    /*
-    str += '<span class = "meta">';
-    str +=   JSON.stringify(logEvent.meta);
-    str += '</span>';
-    */
-    
-  }
-  catch(err) {
-    str += 'Invalid log event!';
-  }
-
-  return str;
-
-}
-
-var months = ['Jan', 'Feb', 'Mar', 'Apr', 
-              'May', 'Jun', 'Jul', 'Aug', 
-              'Sep', 'Oct', 'Nov', 'Dec'];
-
